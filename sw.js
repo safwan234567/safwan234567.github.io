dfbef4c3f5f5655aa7fc06ccf9ea4673666073fe
safwan234567 <safwanshah.work@gmail.com>
2021-05-27 00:23:33 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cbad435a3efb3606254a.js"
+    "url": "webpack-runtime-c1467296849053018b85.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5493c5fefdb080fdad4db22bef67a41d"
+    "revision": "052d5101cb76a338dbfd373d5724489d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "39854ef97e17bc91b965737ca9c38e67"
+    "revision": "958a52f352c1c3649b1fc391f158a94c"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
